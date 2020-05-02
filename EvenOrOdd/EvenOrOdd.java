2324a10bc1a33da7856625c6bd6c90dabd8a0be0
Matt <carmodym2@gmail.com>
2020-05-02 15:04:07 -0600

    Add simple EvenOrOdd

---
+++ EvenOrOdd/EvenOrOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class EvenOrOdd {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Enter an integer number:");
+        int i = scan.nextInt();
+        
+        if (i % 2 == 0) {
+            System.out.println(i + " is even.");
+        } else {
+            System.out.println(i + " is odd.");
+        }
+    }
+}
