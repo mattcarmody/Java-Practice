25f150c10e32cf2dfda2bb594c9278deec9f8e28
Matt <carmodym2@gmail.com>
2020-05-02 20:30:16 -0600

    Add Prime

---
+++ Prime/Prime.java
@@ -0,0 +1,36 @@
+class Prime {
+    
+    public boolean isPrime(int val) {
+        if (val == 0) {
+            return false;
+        } else if (val == 1) {
+            return true;
+        } else if (val == 2) {
+            return true;
+        } else if (val < 0) {
+            System.out.println("For this exercise, negatives are not considered prime.");
+            return false;
+        }
+        
+        
+        for (int jj = 2; jj < val; jj++) {
+            if (val % jj == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {24, 5, 12, -101, 1, 0, 2, 32};
+        Prime prime = new Prime();
+        
+        for (int num : nums) {
+            if (prime.isPrime(num)) {
+                System.out.println(num + " is prime!");
+            } else {
+                System.out.println(num + " is not prime...");
+            }
+        }
+    }
+}
