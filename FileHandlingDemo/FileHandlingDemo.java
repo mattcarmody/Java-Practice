071e577abec2ec5553cf6ecf68159c548ae8178c
Matt <carmodym2@gmail.com>
2020-05-05 21:59:36 -0600

    Add FileHandlingDemo

---
+++ FileHandlingDemo/FileHandlingDemo.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class FileHandlingDemo {
+    
+    public static void main(String[] args) {
+        
+        String line;
+        BufferedReader reader = null;
+        
+        try {
+            reader = new BufferedReader(new FileReader("myFile.txt"));
+            line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
